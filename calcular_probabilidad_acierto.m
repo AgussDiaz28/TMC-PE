439ed12adff56ad5cca326a97dedb0a9ac77c95c
AgussDiaz28 <agussdiaz28@gmail.com>
2017-06-06 23:31:37 -0300

    probabilidad de acierto update

--- calcular_probabilidad_acierto.m
+++ calcular_probabilidad_acierto.m
@@ -1,23 +1,24 @@
+  
+function [probabilidad, todas_las_probabilidades] = calcular_probabilidad_acierto(epsilon,dni)
 
-function [probabilidad, todas_las_probabilidades] = calcular_probabilidad_acierto(espacio, epsilon, radio_disparo)
-
-  probabilidad_anterior = 0; 
-  probabilidad_actual = 1;  
-  casos_favorables = 0;
-  total_de_intentos = 0;
-  todas_las_probabilidades = [];
-  fav = 0;
+      probabilidad_anterior = 0; 
+      probabilidad_actual = 1;  
+      casos_favorables = 0;
+      total_de_intentos = 0;
+      todas_las_probabilidades = [];
+      caso1 = 0;
+      caso2 = 0;
 
       while ~converge(probabilidad_anterior, probabilidad_actual, epsilon, total_de_intentos)
-        probabilidad_anterior = probabilidad_actual;
-          if my_mex_service(38524932) = 1
-            fav = fav + 1;
-            if (fav = 2)
-              casos_favorables = casos_favorables +1;
-              fav = 0; %--- cada dos sucesos favorable
-          else{fav = 0};
-        total_de_intentos = total_de_intentos +1;
-        probabilidad_actual = casos_favorables / total_de_intentos;    
-        todas_las_probabilidades = cat(1, todas_las_probabilidades, probabilidad_actual);
-      end;    
-end;
+            probabilidad_anterior = probabilidad_actual;
+            caso1 = my_mex_service(dni);
+            caso2 = my_mex_service(dni);
+            if ((caso1 == 0) &&( caso2 == 0)) 
+                        casos_favorables = casos_favorables +1;
+            end
+            total_de_intentos = total_de_intentos + 1;
+            probabilidad_actual = casos_favorables / total_de_intentos;    
+            todas_las_probabilidades = cat(1, todas_las_probabilidades, probabilidad_actual);
+      end
+   probabilidad = probabilidad_actual   
+end
